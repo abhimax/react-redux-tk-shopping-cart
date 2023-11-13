a48dd898e8d5b00752ab0a54b4ead57f2b1d25a2
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-11-14 05:20:12 +0530

    feat: data bind with cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,35 +1,50 @@
+import { useSelector } from "react-redux";
 import productList from "../data/productList.json";
 import "../styles/cart.scss";
 
 const Cart = () => {
+  const { cartProductIDs } = useSelector((state) => state.cart);
+  console.log("productList", productList);
+  console.log("cartProductIDs", cartProductIDs);
+  const selectedProducts = productList.products.filter((product) =>
+    cartProductIDs.includes(product.id)
+  );
+  const isCartEmpty = selectedProducts.length < 1;
+  console.log("selectedProducts", selectedProducts);
   return (
     <div className="cart">
-      <div className="cart-product">
-        <h3 className="header">Items in cart</h3>
-        {productList.products.map((product) => (
-          <div key={product.id} className="row">
-            <img className="item-image" src={product.imageUrl} alt="product" />
+      {selectedProducts.length > 0 ? (
+        <div className="cart-product">
+          <h3 className="header">Items in cart</h3>
+          {selectedProducts.map((product) => (
+            <div key={product.id} className="row">
+              <img
+                className="item-image"
+                src={product.imageUrl}
+                alt="product"
+              />
 
-            <div className="item-info">
-              <h4>{product.name}</h4>
-              <p className="text-truncate">{product.detail}</p>
-              <button className="btn btn-primary">
-                <i className="bi bi-trash-fill" /> Remove Item
-              </button>
+              <div className="item-info">
+                <h4>{product.name}</h4>
+                <p className="text-truncate">{product.detail}</p>
+                <button className="btn btn-primary">
+                  <i className="bi bi-trash-fill" /> Remove Item
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
 
-        <footer className="text-center">
-          <button className="btn btn-primary">CHECKOUT</button>
-        </footer>
-      </div>
-
-      <div className="text-center empty-cart">
-        <i className="bi bi-cart3" />
-        <p>Your cart is empty.</p>
-        <p>You have not added any item to your cart.</p>
-      </div>
+          <footer className="text-center">
+            <button className="btn btn-primary">CHECKOUT</button>
+          </footer>
+        </div>
+      ) : (
+        <div className="text-center empty-cart">
+          <i className="bi bi-cart3" />
+          <p>Your cart is empty.</p>
+          <p>You have not added any item to your cart.</p>
+        </div>
+      )}
     </div>
   );
 };
