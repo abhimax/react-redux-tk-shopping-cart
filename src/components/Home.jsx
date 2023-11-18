a38bf81646829c3b7e4fefc6b7953135290c0e68
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-11-19 00:07:16 +0530

    feat: fetch async data with redux

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,21 @@
-import productList from "../data/productList.json";
+//import productList from "../data/productList.json";
+import { useDispatch, useSelector } from "react-redux";
 import "../styles/home.scss";
 import Card from "./Card";
+import { useEffect } from "react";
+import { fetchAllProducts } from "../store/slices/productSlice";
 
 const Home = () => {
+  const { products } = useSelector((state) => state);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAllProducts("http://localhost:3000/products"));
+  }, [dispatch]);
   return (
     <div className="container product-catalogue">
       <div className="row">
-        {productList.products.map((product) => {
+        {products.data?.map((product) => {
           return (
             <Card
               key={product.id}
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./slices/cartSlice";
+import productSlice from "./slices/productSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice.reducer,
+    products: productSlice.reducer,
   },
 });
 export default store;
