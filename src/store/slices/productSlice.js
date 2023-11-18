90f478fad877fe9e4fc2c6e1dda0ea8730271902
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-11-19 00:05:47 +0530

    feat:add product slice

--- src/store/slices/productSlice.js
+++ src/store/slices/productSlice.js
@@ -1,9 +1,33 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const fetchAllProducts = createAsyncThunk(
   "fetch-all-products",
   async (apiUrl) => {
     const response = await fetch(apiUrl);
-    return response.json;
+    return response.json();
   }
 );
+
+const productSlice = createSlice({
+  name: "products",
+  initialState: {
+    data: [],
+    fetchStatus: "",
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllProducts.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.fetchStatus = "success";
+      })
+      .addCase(fetchAllProducts.pending, (state) => {
+        state.fetchStatus = "pending";
+      })
+      .addCase(fetchAllProducts.rejected, (state) => {
+        state.fetchStatus = "error";
+      });
+  },
+});
+
+export default productSlice;
